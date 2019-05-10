1afdca8769381ba3b47f30af985aed9773ac2de7
Darius Seals <tristarmediagroupinc@gmail.com>
2019-05-09 21:12:29 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Ball from './Ball';
+import Lottery from './Lottery'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>LOTTERY</h1>
+      <Lottery />
+      <Lottery title='Pick 4' maxNum={10} numBalls={4} />
+      <Lottery title='Pick 3' maxNum={10} numBalls={3} />
     </div>
   );
 }
